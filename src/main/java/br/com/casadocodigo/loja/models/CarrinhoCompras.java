da6212fb3c793609d7e980065b2757c39d80ba03
augusto <augustosouzads@gmail.com>
2020-06-26 12:58:03 -0300

    add anotação de Scopo no CarrinhoComprasController/CarrinhoCompras branc2

--- src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -3,10 +3,13 @@ package br.com.casadocodigo.loja.models;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component // anotação generica que disponibiliza a classe como um Bean do Spring
-public class CarrinhoCompras {
+@Scope(value=WebApplicationContext.SCOPE_SESSION)//Anotação que comfigura a classe para ser instanciada um objeto para cada usuário(servidor)
+public class CarrinhoCompras {					 //Ex: evitar que um usuario em um outro lugar qualquer compre um item e este seja adicionado ao seu "carrinho".
 	
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<CarrinhoItem, Integer>();
 	
